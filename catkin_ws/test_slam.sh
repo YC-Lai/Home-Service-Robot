b6f33492e3b0e2317e457481e4b532ac037ea4a0
YC Lai <naruto8134@gmail.com>
2021-06-19 14:55:48 +0800

    uplaod own map

--- catkin_ws/test_slam.sh
+++ catkin_ws/test_slam.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+export TURTLEBOT_GAZEBO_WORLD_FILE=/home/workspace/catkin_ws/src/my_robot/worlds/UdacityOffice.world
+export TURTLEBOT_GAZEBO_MAP_FILE=/home/workspace/catkin_ws/src/my_robot/map/map.yaml
 xterm -e " source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
 xterm -e " source devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch" &
