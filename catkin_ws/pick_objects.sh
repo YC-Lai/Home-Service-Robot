ca44b0d1a1b1f65c1c279e3d6b2cbc3aab979072
YC Lai <naruto8134@gmail.com>
2021-06-19 16:01:33 +0800

    fix map

--- catkin_ws/pick_objects.sh
+++ catkin_ws/pick_objects.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-export TURTLEBOT_GAZEBO_WORLD_FILE=/home/workspace/catkin_ws/src/my_robot/worlds/UdacityOffice.world
-export TURTLEBOT_GAZEBO_MAP_FILE=/home/workspace/catkin_ws/src/my_robot/map/map.yaml
+export TURTLEBOT_GAZEBO_WORLD_FILE=/home/workspace/Home-Service-Robot/catkin_ws/src/my_robot/worlds/UdacityOffice.world
+export TURTLEBOT_GAZEBO_MAP_FILE=/home/workspace/Home-Service-Robot/catkin_ws/src/my_robot/maps/map.yaml
+export ROBOT_INITIAL_POSE="-x -4 -y 2 -Y 0"
+
 xterm -e " source devel/setup.bash" &
 xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch " &
 sleep 5
